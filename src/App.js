4d2bda70ea08fe3894dfaff5f539be356d900b6b
mhasanince <mhasanince@gmail.com>
2020-03-05 23:02:51 +0300

    empty app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
