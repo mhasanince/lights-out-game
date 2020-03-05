476ddd3bf96d1fe4ce4a4b6219016e0502b0b8f0
mhasanince <mhasanince@gmail.com>
2020-03-05 22:39:19 +0300

    Updates

---
+++ precache-manifest.24b2f3298174bf9213587f4b36827a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e575977d323c8a98c88d5308f1009109",
+    "url": "/lights-out-game/index.html"
+  },
+  {
+    "revision": "a93e0b9b857ad51e24d9",
+    "url": "/lights-out-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87ece217e6ca3a3dbe69",
+    "url": "/lights-out-game/static/js/2.d70b4fd5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lights-out-game/static/js/2.d70b4fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93e0b9b857ad51e24d9",
+    "url": "/lights-out-game/static/js/main.fdb33e46.chunk.js"
+  },
+  {
+    "revision": "5b2fabad4d260ed5d9e6",
+    "url": "/lights-out-game/static/js/runtime-main.f9c75272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lights-out-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
